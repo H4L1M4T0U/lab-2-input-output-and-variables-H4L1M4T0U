975a00b2ef54cad11a68bf70388f50b638840876
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-09-17 17:50:56 -0400

    ASDASDASDAD
    ASD

--- lab2thing.cpp
+++ lab2thing.cpp
@@ -21,11 +21,16 @@ using namespace std;
 
 int main () {
     string cerealName ;
-    int calorieNum ;
+    double calorieNum ;
     double ouncesPerServing ;
-    int caloriesPERounce ;
-    int Willing2Eat;
+    double caloriesPERounce ;
+    double Willing2Eat;
+    double UcanHave;
 
+
+
+    (caloriesPERounce = calorieNum/ouncesPerServing);   
+    (UcanHave = Willing2Eat/caloriesPERounce); 
 
 
     cout <<  "What Is the name of Your cereal?\n";
@@ -36,24 +41,36 @@ int main () {
     cin >> calorieNum;
 
 
-    cout << "How many calories are you willing to eat? (please round to the nearest whole number im not tryna do allat)";
-    cin >> Willing2Eat;
+
 
 
 
     cout << "Write the number of Ounces here (units not required )\n";
     cin >> ouncesPerServing;
 
+ 
 
- (caloriesPERounce = calorieNum/ouncesPerServing);
+    cout << "How many calories are you willing to eat? (please round to the nearest whole number im not tryna do allat)";
+    cin >> Willing2Eat;
+
 
     cout << "One serving of " << " "<< cerealName  << " is " << " "<< calorieNum << " and has " << " "<< caloriesPERounce << " "<< " calories per serving."  <<endl;
+    cout << "You can have " << UcanHave << "  Servings." ;
 
 
 
 
 
-
+/*
+What Is the name of Your cereal?
+Zion
+Input the number of Calories (dont write the units) 
+12
+Write the number of Ounces here (units not required )
+6
+How many calories are you willing to eat? (please round to the nearest whole number im not tryna do allat)2
+One serving of  Zion is  12 and has  0.99994  calories per serving.
+You can have 6.95191e-310  Servings.*/
 
 
 
